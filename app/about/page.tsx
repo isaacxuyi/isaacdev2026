6d34f4b3b1dac48caed21b0259b4080d0a463417
Isaac Uhunmwangho <isaacco911@gmail.com>
2025-12-24 05:04:40 +0100

    updated about for deploy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,11 @@
-import React from 'react'
+// ... your other imports
 import Header from '../Hero Section/Header/page'
-import Statementxpic from '../componets/StatementsxPic/page'
-import Footer from '../componets/Footer/page'
-import Memberships from '../componets/Memberships/page'
-import Experience from '../componets/Experience/page'
-import Education from '../componets/Education/page'
-import Skills from '../componets/Skills/page'
-import Hobbies from '../componets/Hobbies/page'
 
- const page= () => {
+const page = () => {
   return (
     <div>
-        <Header/>
+        {/* Add the missing prop here */}
+        <Header finishedLoading={true} /> 
         <Statementxpic/>
         <Memberships/>
         <Experience/>
